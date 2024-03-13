e6bb58ed1312633f1d1df42fd8829f09bb7ec87b
Please Lukau <pleaselukau@gmail.com>
2024-03-13 16:20:30 -0600

    Filepath error resolved

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-# main.py
-
+import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
@@ -16,17 +15,25 @@ def preprocess_data(data):
     """
     Preprocess data (e.g., handle missing values, feature scaling).
     """
-    # Example preprocessing steps
-    # Handle missing values
-    data.dropna(inplace=True)
+    # Drop irrelevant columns
+    columns_to_drop = ['Passenger ID', 'First Name', 'Last Name', 'Nationality', 
+                       'Airport Name', 'Airport Country Code', 'Country Name', 
+                       'Departure Date', 'Arrival Airport', 'Pilot Name']
+    data.drop(columns=columns_to_drop, inplace=True)
+    
+    # Encode relevant categorical columns
+    label_encoders = {}
+    columns_to_encode = ['Gender', 'Airport Continent', 'Continents', 'Flight Status']
+    for column in columns_to_encode:
+        label_encoders[column] = LabelEncoder()
+        data[column] = label_encoders[column].fit_transform(data[column])
     
     # Feature scaling
     scaler = StandardScaler()
     features_to_scale = ['Age']  # Numerical features that need scaling
     data[features_to_scale] = scaler.fit_transform(data[features_to_scale])
-    
-    return data
 
+    return data
 
 def train_model(X_train, y_train):
     """
@@ -45,10 +52,19 @@ def evaluate_model(model, X_test, y_test):
     return mse
 
 def main():
-    # Load data
-    data = load_data(r"C:\Users\pleas\OneDrive\Documents\SPRING 2024\CS 4150\business-flight-tracking-trends\data\airline_dataset.csv")
-    print(data.dtypes)
+    
+    # Get the directory of the current script
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
+    # Construct the path to the data file using relative paths
+    data_file_path = os.path.join(current_dir, '..', 'data', 'airline_dataset.csv')
+
+    # Load the data
+    data = load_data(data_file_path)
+
+    # Load data
+    #data = load_data(r"C:\Users\pleas\OneDrive\Documents\SPRING 2024\CS 4150\business-flight-tracking-trends\data\airline_dataset.csv")
+    
     # Preprocess data
     data = preprocess_data(data)
     
